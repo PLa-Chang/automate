37f564b50e46a536a210ecec623dba5a926aaf6a
流觞 <86483506+qwewfc@users.noreply.github.com>
2023-09-25 23:52:56 +0800

    主程序入口

---
+++ MainDrictory/src/Main.java
@@ -0,0 +1,17 @@
+package com.MainDrictory;
+
+
+import com.automate.ProDemo02;
+
+
+/**
+ * 程序入口
+ * @Author: PlaZe、PlaChang、PlaHui、PlaTao
+ * @Date: 2023/9/24 —— 2023/9/28
+ * @goal: 自动化项目
+ */
+public class Main {
+    public static void main(String[] args) {
+        ProDemo02.startThread();
+    }
+}
